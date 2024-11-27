d48240cfc5abfacf73c01b8831abff872333c209
namnt <thanhnam.awa@gmail.com>
2024-11-27 14:16:52 +0700

    feat: add login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from "./auth/login/login.component";
+import {LayoutComponent} from "./layout/layout.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: '', component: LayoutComponent, children: [] },
+  { path: '**', redirectTo: '/login' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,17 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, NavigationEnd, Router} from "@angular/router";
+import {filter} from "rxjs";
 
 @Component({
   selector: 'ord-layout',
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
-export class LayoutComponent {
+export class LayoutComponent implements OnInit{
+  isLoginPage: boolean = false;
+
   isSidebarVisible = false;
   isSidebarMini = false;
   layoutType = 'vertical';  // This could be dynamic
   sidebarType = 'full';     // Can be 'mini-sidebar' or 'full'
   headerPosition = 'fixed'; // Can be dynamic based on your layout
   sidebarPosition = 'fixed'; // Fixed sidebar
+
+  constructor(private router: Router, private activateRoute: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      // Check if the current route is login
+      this.isLoginPage = this.router.url.includes('/login');
+    });
+  }
 
   toggleSidebar() {
     this.isSidebarVisible = !this.isSidebarVisible;
