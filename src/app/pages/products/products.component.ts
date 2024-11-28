d8bc85e8160131123579f1fde5221cf785cb3c62
namnt <thanhnam.awa@gmail.com>
2024-11-28 14:58:08 +0700

    refactor: combine adding and editing form

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -8,57 +8,92 @@ import {ProductsService} from "./products.service";
   styleUrl: './products.component.scss'
 })
 export class ProductsComponent implements OnInit {
-  products: Product[] = [];
-  selectedProduct: Product | null = null;
-
-  defaultNewProduct: Product = {
+  readonly defaultNewProduct: Product = {
     id: Math.floor(Math.random() * 1000),
     code: '',
     name: '',
     category: '',
     soldUnit: 0,
     inStock: 0,
-    expiryDate: new Date('2025-12-31')
+    expiryDate: new Date(Date.now())
   };
-  newProduct: Product = this.defaultNewProduct;
-  isAddingProduct: boolean = false;
+  products: Product[] = [];
+  formProduct: Product | null = null
+
+  isEditing: boolean = false;
 
   constructor(private productService: ProductsService) {}
 
+  /**
+   * Actions to do OnInit
+   */
   ngOnInit(): void {
     this.loadProducts();
   }
 
+  /**
+   * Get products to display
+   */
   loadProducts(): void {
     this.products = this.productService.getAllProducts();
   }
 
+  /**
+   * Add a product
+   */
   onAddProduct(): void {
-    if (this.newProduct) {
+    if (this.formProduct) {
       // Format submitted expiry date
-      this.newProduct.expiryDate = new Date(this.newProduct.expiryDate);
+      this.formProduct.expiryDate = new Date(this.formProduct.expiryDate);
 
-      this.productService.addProduct(this.newProduct);
-      this.newProduct = this.defaultNewProduct;
-      this.isAddingProduct = false
+      this.productService.addProduct(this.formProduct);
+      this.formProduct = this.defaultNewProduct;
+      this.hideAndResetForm();
       this.loadProducts();
     }
   }
 
-  onEditProduct(product: Product) {
-    this.selectedProduct = { ...product }; // Clone product
-  }
-
+  /**
+   * Update a product
+   */
   onUpdateProduct(): void {
-    if (this.selectedProduct) {
-      this.productService.updateProduct(this.selectedProduct);
-      this.selectedProduct = null;
+    if (this.formProduct) {
+      this.productService.updateProduct(this.formProduct);
+      this.hideAndResetForm();
       this.loadProducts();
     }
   }
 
+  /**
+   * Delete a product
+   * @param id Product's ID
+   */
   onDeleteProduct(id: number): void {
     this.productService.deleteProduct(id);
     this.loadProducts();
+  }
+
+  /**
+   * Set up when beginning to add new product
+   */
+  onStartAddingProduct() : void {
+    this.formProduct = this.defaultNewProduct;
+    this.isEditing = false;
+  }
+
+  /**
+   * Set up when beginning to edit a product
+   * @param product Product will be edited
+   */
+  onStartEditingProduct(product: Product): void {
+    this.formProduct = { ...product };
+    this.isEditing = true;
+  }
+
+  /**
+   * Hide and reset form
+   */
+  hideAndResetForm() {
+    this.formProduct = null;
   }
 }
